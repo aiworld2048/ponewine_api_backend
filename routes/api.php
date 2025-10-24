535fa7d78bab6f085195e1ff73dd9a7863b186d2
AMK1215 <aungmyokhaing1215@gmail.com>
2025-10-24 10:52:25 +0630

    second update

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,13 @@ use App\Http\Controllers\Api\V1\Shan\ShanGetBalanceController;
 use App\Http\Controllers\Api\PoneWine\PoneWineClientBalanceUpdateController;
 use App\Http\Controllers\Api\PoneWine\PoneWineLaunchGameController;
 use App\Http\Controllers\Api\PoneWine\ProviderLaunchGameController;
+use App\Http\Controllers\Api\V1\Auth\AuthController;
 
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/player-change-password', [AuthController::class, 'playerChangePassword']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::group(['prefix' => 'shan'], function () {
     Route::post('balance', [ShanGetBalanceController::class, 'getBalance']);
