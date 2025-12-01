50401e241dd6514290a651ca165a52d179b441fc
AMK1215 <aungmyokhaing1215@gmail.com>
2025-12-01 10:59:53 +0630

    azm player site url remove

--- config/buffalo_sites.php
+++ config/buffalo_sites.php
@@ -2,13 +2,13 @@
 
 $defaultProviderApiUrl = env('BUFFALO_API_URL', 'https://api-ms3.african-buffalo.club/api/game-login');
 $defaultDomain = env('BUFFALO_DOMAIN', 'prime.com');
-$defaultGameServerUrl = env('BUFFALO_GAME_SERVER_URL', null); // https://prime.next-api.net
+$defaultGameServerUrl = env('BUFFALO_GAME_SERVER_URL', 'https://prime.next-api.net');
 $TTT_Lobby_Url = env('TTT_LOBBY_URL', 'https://tttgamingmm.pro');
 $OneXBet_Lobby_Url = env('ONEXBET_LOBBY_URL', 'https://m.onexbetmm.site');
 $Burmese888_Lobby_Url = env('BURMESE888_LOBBY_URL', 'https://m.burmar888.site');
 $Shanyoma_Lobby_Url = env('SHANYOMA_LOBBY_URL', 'https://m.shanyoma789.com');
-$AZM999_Lobby_Url = env('AZM999_LOBBY_URL', null); // https://online.azm999.com
-$AZM999_GameServer_Url = env('AZM999_GAME_SERVER_URL', null);
+$AZM999_Lobby_Url = env('AZM999_LOBBY_URL', 'https://online.azm999.com'); // https://online.azm999.com
+$AZM999_GameServer_Url = env('AZM999_GAME_SERVER_URL', 'https://online.azm999.com');
 $defaultGameId = env('BUFFALO_GAME_ID', 23);
 $defaultApiTimeout = env('BUFFALO_API_TIMEOUT', 30);
 
